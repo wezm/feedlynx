5c0720522a78c3e5e85b4537128cf30f42b3e1ac
Wesley Moore <wes@wezm.net>
2024-06-22 12:00:37 +1000

    Add functions to detect YouTube videos

--- src/feed.rs
+++ src/feed.rs
@@ -1,6 +1,6 @@
-use std::fs::File;
 use std::io::BufReader;
 use std::path::PathBuf;
+use std::{borrow::Cow, fs::File};
 
 use atom_syndication::{self as atom, Generator};
 use chrono::{DateTime, Utc};
@@ -73,5 +73,93 @@ impl Feed {
         // // Move into place atomically
         // fs::rename(cookie_store_tmp_path, cookie_store_path).map_err(Error::from)
         todo!()
+    }
+}
+
+fn is_youtube(url: &URI) -> bool {
+    let Some(host) = url.host() else {
+        return false;
+    };
+    match host {
+        uriparse::Host::IPv4Address(_) => false,
+        uriparse::Host::IPv6Address(_) => false,
+        uriparse::Host::RegisteredName(name) => matches!(
+            name.as_str(),
+            "www.youtube.com" | "youtu.be" | "m.youtube.com" | "youtube-nocookie.com"
+        ),
+    }
+}
+
+fn is_short(url: &URI) -> bool {
+    let Some(host) = url.host() else {
+        return false;
+    };
+    match host {
+        uriparse::Host::IPv4Address(_) => false,
+        uriparse::Host::IPv6Address(_) => false,
+        uriparse::Host::RegisteredName(name) => name == "youtu.be",
+    }
+}
+
+fn youtube_video_id<'a>(url: &'a URI) -> Option<Cow<'a, str>> {
+    // Try for v param, fall back on 'v' segment
+    let id = url
+        .query()
+        .and_then(|q| {
+            form_urlencoded::parse(q.as_bytes()).find_map(|(key, value)| {
+                if key == "v" {
+                    Some(value)
+                } else {
+                    None
+                }
+            })
+        })
+        .or_else(|| match url.path().segments() {
+            [first, id] if first == "v" => Some(Cow::Borrowed(id.as_str())),
+            [id] if is_short(url) => Some(Cow::Borrowed(id.as_str())),
+            _ => None,
+        });
+
+    id
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_video_id_direct() {
+        let url = URI::try_from("https://www.youtube.com/watch?v=u1wfCnRINkE").unwrap();
+        assert!(is_youtube(&url));
+        assert_eq!(youtube_video_id(&url).unwrap(), "u1wfCnRINkE");
+    }
+
+    #[test]
+    fn test_video_id_short() {
+        let url = URI::try_from("https://youtu.be/u1wfCnRINkE").unwrap();
+        assert!(is_youtube(&url));
+        assert_eq!(youtube_video_id(&url).unwrap(), "u1wfCnRINkE");
+    }
+
+    #[test]
+    fn test_video_id_fullscreen() {
+        let url = URI::try_from("https://www.youtube.com/v/u1wfCnRINkE").unwrap();
+        assert!(is_youtube(&url));
+        assert_eq!(youtube_video_id(&url).unwrap(), "u1wfCnRINkE");
+    }
+
+    #[test]
+    fn test_video_id_fullscreen_param() {
+        let url = URI::try_from("https://www.youtube.com/v/u1wfCnRINkE?version=3").unwrap();
+        assert!(is_youtube(&url));
+        assert_eq!(youtube_video_id(&url).unwrap(), "u1wfCnRINkE");
+    }
+
+    #[test]
+    fn test_video_id_channel_url() {
+        let url =
+            URI::try_from("https://www.youtube.com/channel/UCLi0H57HGGpAdCkVOb_ykVg").unwrap();
+        assert!(is_youtube(&url));
+        assert_eq!(youtube_video_id(&url), None);
     }
 }
